9ee48f86a11f4356dceebca5a90c0849669fd836
Angelica Aluo <angelicaaluo@Angelicas-Air.lan>
2023-06-20 15:28:37 +0100

    updates file

--- database_utils.py
+++ database_utils.py
@@ -1,12 +1,10 @@
 import psycopg2
-import yaml
-
-
+import yaml, pandas as pd
+ 
 # Class to connect to database using psycopg2
 class DatabaseConnector:
     def __init__(self,yaml_file):
         self.yaml_file = yaml_file
-        pass
 
     def read_creds(self):
         with open(self.yaml_file, 'r') as file:
@@ -14,8 +12,8 @@ class DatabaseConnector:
             return creds
     
     def establish_conn(self):
-        creds = self.read_creds()
-        conn = psycopg2.connect(user =creds['RDS_USER'],password=creds['RDS_PASSWORD'],host =creds['RDS_HOST'],port =creds['RDS_PORT'],dbname= creds['RDS_DATABASE'])
+        self.creds = self.read_creds()
+        conn = psycopg2.connect(user =self.creds['RDS_USER'],password=self.creds['RDS_PASSWORD'],host =self.creds['RDS_HOST'],port =self.creds['RDS_PORT'],dbname= self.creds['RDS_DATABASE'])
         cur = conn.cursor() 
         return cur
 
@@ -26,6 +24,19 @@ class DatabaseConnector:
         cur.execute(query)
         for table in cur.fetchall():
             print(table)
+
+    # def upload_to_db(self,df,table):
+    #     # df = pd.DataFrame()
+    #     conn = self.establish_conn()
+    #     try:
+    #         conn.execute(df.to_sql(name=table, con=conn, if_exists='replace', index=False))
+    #         conn.close()
+    #         # df.to_sql(name=table, con=conn, if_exists='replace', index=False)
+    #         print(f"Data uploaded to '{table}' table successfully.")
+    #     except Exception as e:
+    #         print(f"Error uploading data to '{table}' \n {e}")
+    #     # conn.execute(df.to_sql(name=table, con=conn, if_exists='replace', index=False))
+    #     conn.close()
 
 
 if __name__ == '__main__':
