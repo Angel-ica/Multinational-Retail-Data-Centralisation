8a0b52d9f7904516562f57799bba42963300c357
Angelica Aluo <angelicaaluo@Angelicas-Air.lan>
2023-06-20 15:28:06 +0100

    updates file

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,11 @@
-import pandas as pd,yaml
+import pandas as pd
+import yaml
 import random
 import sqlalchemy as db
+import PyPDF2 as pdf
+import requests,os
+from data_cleaning import DataCleaning
+
 
 # Class to extract data using Sqlalchemy
 
@@ -31,12 +36,42 @@ class DataExtractor:
         # print(df.columns)
         return df
     
+    def upload_to_db(self,df,table):
+        creds = self.read_creds()
+        db_uri = f"postgresql+psycopg2://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
+        engine = db.create_engine(db_uri)
+        conn = engine.connect()  
+        df.to_sql(name=table,con=conn, if_exists='replace') 
+
+    def retrieve_pdf_data(self,url):
+        data = ''
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open('temp.pdf', 'wb') as file:
+                file.write(response.content)
+
+            with open('temp.pdf', 'rb') as file:
+                reader = pdf.PdfFileReader(file)
+                for page in reader.pages:
+                    text = page.extract_text()
+                    data +=text
+
+            os.remove('temp.pdf')
+        else:
+            print(f"Error retrieving PDF data. Status code: {response.status_code}")
+        return data
+
 
 if __name__=='__main__':
 
     def main():
         dex = DataExtractor('db_creds.yaml')
         table = dex.read_rds_table('legacy_users')
-        print(table)
+        file = dex.retrieve_pdf_data('https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf')
+        clean_cd = DataCleaning('legacy_users')
+        clean_cd.clean_card_data(file)
+        print(clean_cd)
     main()
 
+#TODO
+'Carry out all tasks in a main function to resolve conflicts'
